e64fc229fcddc3509b195ac4c91236a34494cbca
Pranav K <pranavkcse@gmail.com>
2024-08-05 21:14:16 +0530

    using css styles

--- react-fundementals/src/index.js
+++ react-fundementals/src/index.js
@@ -1,14 +1,48 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-function Greeting() {
+import "./index.css";
+
+function BookList() {
   return (
-    <>
-      <h2 className="something"></h2>
-    </>
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
   );
 }
 
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => (
+  <img
+    src="https://images-na.ssl-images-amazon.com/images/I/81O6JfipQhL._AC_UL600_SR600,400_.jpg"
+    alt="img"
+  />
+);
+const Title = () => {
+  return <h2>The Book of Bill</h2>;
+};
+const Author = () => {
+  const inlineStyles = {
+    color: "#617d98",
+    fontSize: "0.75rem",
+    marginTop: "0.5rem",
+  };
+
+  return <h4 style={inlineStyles}>Author</h4>;
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Greeting />);
+root.render(<BookList />);
