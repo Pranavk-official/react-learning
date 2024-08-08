345db1d75d99e74e9eaff75a2313578bd533ac6e
Pranav K <pranavkcse@gmail.com>
2024-08-08 13:45:43 +0530

    Completed spliting up the components and configured the dynamic setup

--- 02-backroads-app/src/components/index.js
+++ 02-backroads-app/src/components/index.js
@@ -3,6 +3,7 @@ import Footer from "./Footer";
 import Hero from "./Hero";
 import Navbar from "./Navbar";
 import Services from "./Services";
+import Title from "./Title";
 import Tour from "./Tour";
 
-export { Navbar, Hero, About, Footer, Services, Tour };
+export { Navbar, Hero, About, Footer, Services, Tour, Title };
